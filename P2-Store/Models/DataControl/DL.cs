9cb9b0c0d6c019e446eccd657d10407a1a5d5c52
Jorge <jorgeyumiseba@gmail.com>
2021-09-15 17:35:31 -0400

    Database Scaffolding

---
+++ P2-Store/Models/DataControl/DL.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using P2_Store.Models.Interfaces;
+
+namespace P2_Store.Models.DataControl
+{
+    public class DL : IDL
+    {
+
+    }
+}
