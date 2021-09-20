0bb655722a67c880242e9ffd6070ee0ea9c3e1ba
Ehart <Ehart3@masonlive.gmu.edu>
2021-09-20 16:53:57 -0400

    Added coverage to Karam, deleted unused imports

--- project2-app/src/app/app.module.ts
+++ project2-app/src/app/app.module.ts
@@ -2,10 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { SharedModule } from './shared-module/shared.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { HeaderComponent } from './components/header/header.component';
-import { SearchbarComponent } from './components/searchbar/searchbar.component';
-import { ProductsComponent } from './components/products/products.component';
 
 
 @NgModule({
